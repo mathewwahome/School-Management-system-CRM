53f52df3355c18c0aa3e8297881fd8882cb86557
wa2f <mathewwahome2003@gmail.com>
2022-07-21 18:42:28 +0300

    ok

--- index.php
+++ index.php
@@ -16,45 +16,50 @@
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Newsreader:ital,wght@0,600;1,600&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,300;0,500;0,600;0,700;1,300;1,500;1,600;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,400;1,400&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
 </head>
 <body>
 <header>
-    <!--    bootstrap header-->
-    <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#" style="font-size: 30px">
-            <font style="color: deeppink;font-family: 'Poppins', 'Montserrat', sans-serif">X-</font>School</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse " id="collapsibleNavbar">
-                <ul class="navbar-nav navnavfloat">
-                    
-
-                    <div class="btn-group mr-2">
-                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">Signup</button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="teacherregister.php">As Teacher</a></li>
-                            <li><a class="dropdown-item" href="signup.php">As Student</a></li>
-                        </ul>
-                    </div>
-                    <div class="btn-group">
-                        <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">Login</button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="teacher.php">As Teacher</a></li>
-                            <li><a class="dropdown-item" href="login.php">As Student</a></li>
-                        </ul>
-                    </div>
-                </ul>
+   <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav">
+            <div class="container px-5">
+                <a class="navbar-brand fw-bold" href="#page-top">WG</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        Menu
+                    <i class="bi-list"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                        <li class="nav-item"><a class="nav-link me-lg-3" href="login.php">Login</a></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3" href="signup.php">Signup</a></li>
+                    </ul>
+                    <button class="btn btn-primary rounded-pill px-3 mb-2 mb-lg-0" data-bs-toggle="modal" data-bs-target="#feedbackModal">
+                        <span class="d-flex align-items-center">
+                            <i class="bi-chat-text-fill me-2"></i>
+                            <span class="small">Send Message</span>
+                        </span>
+                    </button>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
 </header>
 <section>
     <div class="body_head">
-
+        <h1>heloo</h1>
     </div>
-
+</section>
+<section>
+    <div class="next_section">
+        <h1>heloo</h1>
+    </div>
 </section>
 <section>
     <center>
